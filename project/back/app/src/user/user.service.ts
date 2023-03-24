8c725042bebe26760f0c385b634f0e3247ccdbbf
gchatain <gchatain@student.42lyon.fr>
2023-03-24 17:06:12 +0100

    protection creation de user bdd

--- project/back/app/src/user/user.service.ts
+++ project/back/app/src/user/user.service.ts
@@ -61,6 +61,9 @@ export class UserService {
         user.id = retUser.id;
         user.username = retUser.login;
         user.avatar_url = retUser.image.link;
+        if (await this.userRepository.findOneBy({id : user.id}) != null) {
+            return null;
+        }
         await this.userRepository.save(user);
         return user;
     }
