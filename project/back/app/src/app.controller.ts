304c924d4580f4a06b03023791528016bff68d43
gchatain <gchatain@student.42lyon.fr>
2023-03-24 15:04:47 +0100

    ajout url simple pour front + mise en .env donnée app

--- project/back/app/src/app.controller.ts
+++ project/back/app/src/app.controller.ts
@@ -10,6 +10,4 @@ export class AppController {
     return this.appService.getHello();
   }
 
-
-  
 }
--- project/back/app/src/user/user.controller.ts
+++ project/back/app/src/user/user.controller.ts
@@ -6,14 +6,22 @@ export class UserController {
     constructor(private readonly userService: UserService) {}
 
     @Get('login')
+    redirectLogin(@Res() res) {
+      res.redirect('https://api.intra.42.fr/oauth/authorize?client_id=' + process.env.APP_ID + '&redirect_uri=' + process.env.APP_REDIRECT_URI + '&response_type=code');
+    }
+    
+
+    @Get('login/token')
     getLogin(@Query() id, @Res() res) : string {
       if (id.code == null) {
         res.status(400).send('Bad Request');
       }
       this.userService.createUsers(id.code);
       res.redirect('https://intra.42.fr');
+      //res.status(200).send('OK');
       return id;
     }
+
 
     @Get()
     getUsers() {
