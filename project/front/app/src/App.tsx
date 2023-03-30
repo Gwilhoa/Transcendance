a18ee0094c7fd40f5d18d5a5df4a8e74b1e861de
fdaumas <florian.daumas.git@gmail.com>
2023-03-30 14:37:52 +0200

    start app.tsx

--- project/front/app/src/App.tsx
+++ project/front/app/src/App.tsx
@@ -4,21 +4,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="http://localhost:6200/auth/login"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-		    Authentification
-        </a>
-      </header>
+	<div className="App">
+		<div>
+			<header>
+				<h1>
+					Transcendence
+				</h1>
+			</header>
+			<a
+			className="App-link"
+			href="http://localhost:6200/auth/login"
+			target="_blank"
+			rel="noopener noreferrer"
+	        >
+			    Authentification
+			</a>
+		</div>
     </div>
   );
 }
