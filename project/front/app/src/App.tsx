645db1b18ad1a48dc9e6e092704dcf0826faf43b
Aurele Josserand <ajossera@z2r11p2.42lyon.fr>
2023-03-31 19:02:36 +0200

    route

--- project/front/app/src/App.tsx
+++ project/front/app/src/App.tsx
@@ -1,26 +1,73 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Begin from './components/begin'
+
+
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+function Template(props: Props) {
+  return (
+    <div>
+      <header>Header goes here</header>
+      <main>{props.children}</main>
+      <footer>Footer goes here</footer>
+    </div>
+  );
+}
+
+
+
+const Authentificate = () => {
+    return (<div className="App">
+    <div>
+        <header>
+            <h1>
+                Transcendence
+            </h1>
+        </header>
+        <a
+        className="App-link"
+        href="http://localhost:6200/auth/login"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+            Authentification
+        </a>
+    </div>
+    </div>)
+};
+
+
+const Chat = () => {
+	return (
+		<Template>
+			<h1> sfsdsfs </h1>
+		</Template>
+
+	)
+
+}
+
+const maBiteEstGrosse = () => {
+	return (null)
+}
+
 
 function App() {
   return (
-	<div className="App">
-		<div>
-			<header>
-				<h1>
-					Transcendence
-				</h1>
-			</header>
-			<a
-			className="App-link"
-			href="http://localhost:6200/auth/login"
-			target="_blank"
-			rel="noopener noreferrer"
-	        >
-			    Authentification
-			</a>
-		</div>
-    </div>
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" Component={Authentificate} />
+			<Route path="/chat" Component={Chat} />
+			<Route path="/accueil" Component={maBiteEstGrosse} />
+		</Routes>
+	</BrowserRouter>
   );
 }
 
