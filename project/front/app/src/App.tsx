3b6089ec3c5a54e96656dbf7e9bf1e946d47ee83
gchatain <guilheimchataing@gmail.com>
2023-04-03 18:40:48 +0200

    Add files via upload
    front working

---
+++ project/front/app/src/App.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function GetTokenUser(props: { url: string }) {
+  const [body, setBody] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch(props.url)
+      .then((response) => response.text())
+      .then((text) => setBody(text));
+  }, []);
+
+  if (!body) {
+    return <div>Loading...</div>;
+  }
+
+  return <div dangerouslySetInnerHTML={{ __html: body }} />;
+}
+
+function App() {
+  return (
+	<div className="App">
+		<h1>Transcendence</h1>
+		<a
+		className="api42-link"
+		href="http://localhost:6200/auth/login"
+		target="_blank"
+		rel="noopener noreferrer"
+		>
+			Authentification
+		</a>
+			<GetTokenUser url="http://localhost:6200/auth/login"/>
+    </div>
+  );
+}
+
+export default App;
