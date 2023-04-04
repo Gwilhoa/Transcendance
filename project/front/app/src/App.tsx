fed9fc854aa93abd1beda4014005e0bc6c7171ec
fdaumas <florian.daumas.git@gmail.com>
2023-04-04 06:00:33 +0200

    clean code

--- project/front/app/src/App.tsx
+++ project/front/app/src/App.tsx
@@ -1,23 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function GetTokenUser(props: { url: string }) {
-  const [body, setBody] = useState<string | null>(null);
-
-  useEffect(() => {
-    fetch(props.url)
-      .then((response) => response.text())
-      .then((text) => setBody(text));
-  }, []);
-
-  if (!body) {
-    return <div>Loading...</div>;
-  }
-
-  return <div dangerouslySetInnerHTML={{ __html: body }} />;
-}
-
 function App() {
+  //state
+
+  //comportement
+	const GetTokenUser = (props: { url: string }) => {
+		const [body, setBody] = useState<string | null>(null);
+
+		useEffect(() => {
+				fetch(props.url)
+				.then((response) => response.text())
+				.then((text) => setBody(text));
+				}, []);
+
+		if (!body) {
+			return <div>Loading...</div>;
+		}
+
+		return <div dangerouslySetInnerHTML={{ __html: body }} />;
+	}
+
+  //render	
   return (
 	<div className="App">
 		<h1>Transcendence</h1>
