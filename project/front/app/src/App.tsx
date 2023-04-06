9aa1762e755295a071e4128fb1d3256a7bc55ea4
fdaumas <florian.daumas.git@gmail.com>
2023-04-06 18:39:07 +0200

    Auth

--- project/front/app/src/App.tsx
+++ project/front/app/src/App.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from './pages/Auth';
+import AuthToken from './pages/AuthToken';
 import NotFound from './pages/NotFound';
 import Game from './pages/game';
 import Profil from './pages/profil';
@@ -14,6 +15,7 @@ function App() {
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" Component={Auth}/>
+				<Route path="/auth" Component={AuthToken}/>
 				<Route path="*" Component={NotFound}/>
 				<Route path="/accueil" Component={Accueil} />
       			<Route path="/game" Component={Game} />
