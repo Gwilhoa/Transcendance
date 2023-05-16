cdc7620119db82a45f24ed6dd48afba3322e96ea
fdaumas <florian.daumas.git@gmail.com>
2023-05-16 15:32:29 +0200

    start print of qrCode

--- project/front/app/src/pages/CreateTwoFa.tsx
+++ project/front/app/src/pages/CreateTwoFa.tsx
@@ -1,13 +1,51 @@
-import { ReactNode } from "react";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import AuthCode from 'react-auth-code-input';
+import "../style/CreateTwoFa.css";
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
 
 const CreateTwoFaPage = () => {
 		 
-        return (
-            <>
-                <h1> TWOFA </h1>
-            </>
-    
-        );
+	const [result, setResult] = useState<string>("");
+	const [Error, setError] = useState<string>("");
+	const handleOnChange = (res: string) => {
+		setResult(res);
+	};
+
+	useEffect(() => {
+		if (!result) {
+			axios.get("http://localhost:3000/auth/2fa/create", {
+				headers: {
+				Authorization: `Bearer ${cookies.get('jwtAuthorization')}`,
+				},
+			})
+				.then((response) => {
+					console.log(response);
+					setResult(response.data);
+				})
+				.catch((error) => {
+					setError("Error " + error.response.status);
+					console.error("profil Error status " + error.response.status);
+					console.error(error);
+				});
+		}
+	}, []);
+
+	return (
+		<>
+			<h1> TWOFA </h1>
+			<img src={result} />
+			<div>
+				<AuthCode 
+					allowedCharacters='numeric' 
+					onChange={handleOnChange} 
+					inputClassName='input'
+				/>
+			</div>
+		</>
+
+	);
 }
 
 export default CreateTwoFaPage;
