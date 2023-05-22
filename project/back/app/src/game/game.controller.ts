55ddac3692c227621370fe00be69a0a552720d5b
gchatain <gchatain@student.42lyon.fr>
2023-05-22 17:18:29 +0200

    base 64 image théoriquement les gifs et png peuvent toujours marcher

--- project/back/app/src/game/game.controller.ts
+++ project/back/app/src/game/game.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtIsAuthGuard } from 'src/auth/guard/jwt.guard';
 import { CreateGameDTO } from 'src/dto/create-game.dto';
 import { GameService } from './game.service';
@@ -10,17 +18,17 @@ export class GameController {
   constructor(private readonly gameService: GameService) {}
 
   @Get()
-  getGames(@GetUser('sub') id: string) {
-    return this.gameService.getGames(id);
+  getGames(@GetUser('sub') id: string, @Res() response) {
+    return response.status(200).send(this.gameService.getGames(id));
   }
 
   @Post()
-  createGame(@Body() body: CreateGameDTO) {
-    return this.gameService.createGame(body);
+  createGame(@Body() body: CreateGameDTO, @Res() response) {
+    return response.status(200).send(this.gameService.createGame(body));
   }
 
   @Get('/id/:id')
-  getGameById(@Param('id') id: string) {
-    return this.gameService.getGameById(id);
+  getGameById(@Param('id') id: string, @Res() response) {
+    return response.status(200).send(this.gameService.getGameById(id));
   }
 }
