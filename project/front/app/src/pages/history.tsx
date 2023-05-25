7a5c6a9c61f6a503bb9ebee43d13f191bc575dd1
Henri Penaud <henrialsace@laposte.net>
2023-05-25 18:48:28 +0200

    history working back

--- project/front/app/src/pages/history.tsx
+++ project/front/app/src/pages/history.tsx
@@ -2,25 +2,19 @@ import React, { useEffect, useState }from "react";
 import './css/history.css'
 import ErrorToken, { setErrorLocalStorage } from '../components/IfError';
 import axios from '../components/utils/API'
-import { AxiosResponse } from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { cookies } from '../App'
-import { off } from "process";
 
-interface Score {
-    ennemy: string;
-    scoreEnnemy: number;
-    scoreMe: number;
-}
- 
 
 interface ShowScore {
     status: string;
-    score1: string;
-    score2: string;
+    myScore: string;
+    opponentScore: string;
 }
 
-const OneScoreBlock = ({status, score1, score2 }: ShowScore) => {
+const vistoryScore = 3;
+
+const OneScoreBlock = ({status, myScore, opponentScore }: ShowScore) => {
     return (
       <div className="score-block">
         <h3 className="status">{status}</h3>
@@ -28,13 +22,13 @@ const OneScoreBlock = ({status, score1, score2 }: ShowScore) => {
             <div className="player">
                 {/* <a href="" className="image"><img className="image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/42_Logo.svg/1200px-42_Logo.svg.png"></img></a> */}
                 <img className="image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/42_Logo.svg/1200px-42_Logo.svg.png"></img>
-                <p>{score1}</p> {/* todo: add link to profil */}
+                <p>{myScore}</p> {/* todo: add link to profil */}
             </div>
             <p>against</p>
             <div className="player">
                 {/* <a href="" className="image"><img className="image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/42_Logo.svg/1200px-42_Logo.svg.png"></img></a> */}
                 <img className="image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/42_Logo.svg/1200px-42_Logo.svg.png"></img>
-                <p>{score2}</p> {/* todo: add link to profil */}
+                <p>{opponentScore}</p> {/* todo: add link to profil */}
             </div>
         </div>
       </div>
@@ -54,7 +48,6 @@ function getUserIndex(game : any)
 const Add = () => {
     const blocks = [];
     const [response, setResponse] = useState<any>(null);
-    // todo: change enemy here no more use
 
     const navigate = useNavigate();
     useEffect(() =>{
@@ -73,50 +66,44 @@ const Add = () => {
             navigate('/Error');
         })
     }, [navigate]);
+    console.log("toto");
     
-    const ListOfScore: Score[] = [
-        { ennemy: "Gchatain", scoreEnnemy: 3, scoreMe: 25 },
-        { ennemy: "Dieu", scoreEnnemy: 50, scoreMe: 49 },
-        { ennemy: "Xav Niel", scoreEnnemy: 0, scoreMe: 100 },
-        { ennemy: "Gchatain", scoreEnnemy: 3, scoreMe: 25 },
-        { ennemy: "Dieu", scoreEnnemy: 50, scoreMe: 49 },
-        { ennemy: "Xav Niel", scoreEnnemy: 0, scoreMe: 100 },
-
-      ];
     if (response == null)
-      return <></>;
-    console.log('-------------------------');
+    {
+        console.log("toto");
+        return (<p className="no-game-played">{"You don't apost have played a game yet!"}</p>);
+    }
+    let i = 0;
     for (const game of response)
     {
-        console.log('game :')
-        console.log(game)
-        console.log('user id: ' + getUserIndex(game));
+        let status : string;
+        if( game.score1 == vistoryScore && getUserIndex(game) == 1)
+            status = 'Victory';
+        else
+            status = 'Defeat';
+        let myScore : string;
+        let opponentScore : string;
+        if (getUserIndex(game) == 1)
+        {
+            myScore = game.score1;
+            opponentScore = game.score2;
+        }
+        else
+        {
+            myScore = game.score2;
+            opponentScore = game.score1;
+        }
+        blocks.push (
+            <OneScoreBlock
+            status={status + ':'}
+            myScore={myScore}
+            opponentScore={opponentScore}
+            key={i}
+            />
+        )
+        i++;
     }
-    ////// REMPLIR LIST OF SCORE AVEC LES VRAIS SCORES ///////////
-    
-    for (let i = 0; i < ListOfScore.length; i++) {
-        if (ListOfScore[i].scoreMe > ListOfScore[i].scoreEnnemy) {
-            blocks.push (
-                <OneScoreBlock
-                status={"Victory:"}
-                score1={String(ListOfScore[i].scoreMe)}
-                score2={String(ListOfScore[i].scoreEnnemy)}
-                key={i}
-                />
-            )
-        }
-        else { 
-            blocks.push (
-                <OneScoreBlock
-                status={"Defeat:"}
-                score1={String(ListOfScore[i].scoreMe)}
-                score2={String(ListOfScore[i].scoreEnnemy)}
-                key={i}
-                />
-            )
-            }
-        }
-        return <div className="score-board">{blocks}</div>;
+    return <div className="score-board">{blocks}</div>;
 }
 
 const History = () => {
@@ -129,7 +116,7 @@ const History = () => {
                 </div>
             </header>
             <div className="scrollBlock">
-                <Add/>
+                <Add />
             </div>
         </div>
     );
