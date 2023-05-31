c1ef12d273da3b4869f41f61b6d2ee92bb56edcf
Staneo644 <aurele.josserand@gmail.com>
2023-05-31 15:16:51 +0200

    racket move

--- project/front/app/src/pages/game.tsx
+++ project/front/app/src/pages/game.tsx
@@ -18,8 +18,8 @@ const Game = () => {
   const [score2, setScore2] = useState(0);
   
   const [ball, setBall] = useState({ x: 50, y: 50});
-  const [paddle1, setPaddle1] = useState({ y: 42.5 });
-  const [paddle2, setPaddle2] = useState({ y: 42.5 });
+  const [paddle1, setPaddle1] = useState(42.5 );
+  const [paddle2, setPaddle2] = useState(42.5 );
   const navigate = useNavigate();
   
   
@@ -30,6 +30,7 @@ const Game = () => {
       case "KeyW":
         //setPaddle1((paddle) => ({ y: (paddle.y - 1) < 0 ? 0: paddle.y - 1}));
         socket.emit("input_game", {game_id: gameId, type: 1})
+        setPaddle1(paddle1 + 1);
         console.log(gameId);
         break;
       case "KeyS":
@@ -100,14 +101,17 @@ const Game = () => {
     console.log(any);
     //setGameId(any)
   })
+  
 
   useEffect(() => {
     socket.on('update_game', (data) => {
       setScore1(data.score1);
       setScore2(data.score2);
       setBall({x: data.ballx, y: data.bally});
-      setPaddle2(data.rack2y);
+      setPaddle2(data.rack2y)
       setPaddle1(data.rack1y);
+      console.log(paddle2);
+      console.log(paddle1);
       if (!onGame) {
         findGame(true);
       }
@@ -157,8 +161,8 @@ const Game = () => {
 
 
   
-          <div className="paddle1" style={{ top: paddle1.y + '%' }} />
-          <div className="paddle2" style={{ top: paddle2.y + '%'}} />
+          <div className="paddle1" style={{ top: paddle1 + '%' }} />
+          <div className="paddle2" style={{ top: paddle2 + '%'}} />
           <div className="ball" style={{ top: ball.x + '%', left: ball.y + '%' }} />
         </>
       }
