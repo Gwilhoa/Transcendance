bf529739295b6cd7d951660d214e4c8cac526697
gchatain <gchatain@student.42lyon.fr>
2023-06-05 16:22:49 +0200

    game retry

--- project/back/app/src/events/Game.class.ts
+++ project/back/app/src/events/Game.class.ts
@@ -7,13 +7,13 @@ export class Game {
   static default_positionBx = this.default_sizeMaxX / 2;
   static default_sizeMaxY = 100;
   static default_positionBy = this.default_sizeMaxY / 2;
-  static default_update = 60;
+  static default_update = 16;
   static default_racklenght = 15;
-  static default_positionR = 50 - (this.default_racklenght / 2);
+  static default_positionR = 50 - this.default_racklenght / 2;
   static default_rackwidth = 2;
   static default_steprack = 1;
   static default_radiusball = 1;
-  static default_speedBall = 2;
+  static default_speedBall = 0.5;
   static default_sizeMinX = 0;
   static default_sizeMinY = 0;
   static default_victorygoal = 6;
@@ -29,7 +29,7 @@ export class Game {
   private _score2: number;
   private _ballx: number;
   private _bally: number;
-  private _speedball : number;
+  private _speedball: number;
   private _angle;
   private _minX;
   private _maxX;
@@ -111,25 +111,19 @@ export class Game {
       if (y == 1) {
         if (this._rack1y >= this._maxY - Game.default_racklenght)
           this._rack1y = this._maxY - Game.default_racklenght;
-        else
-          this._rack1y += 2;
+        else this._rack1y += 2;
       } else if (y == 0) {
-        if (this._rack1y - 2 <= 0)
-          this._rack1y = 0 ;
-        else
-          this._rack1y -= 2;
+        if (this._rack1y - 2 <= 0) this._rack1y = 0;
+        else this._rack1y -= 2;
       }
     } else if (player.id == this._user2.id) {
       if (y == 1) {
         if (this._rack2y >= this._maxY - Game.default_racklenght)
           this._rack2y = this._maxY - Game.default_racklenght;
-        else
-          this._rack2y += 2;
+        else this._rack2y += 2;
       } else if (y == 0) {
-        if (this._rack2y <= 0)
-          this._rack2y = 0;
-        else
-          this._rack2y -= 2;
+        if (this._rack2y <= 0) this._rack2y = 0;
+        else this._rack2y -= 2;
       }
     }
   }
@@ -148,35 +142,37 @@ export class Game {
   public async start() {
     console.log('game launch ' + this._id);
     await sleep(3000);
-    while (((Math.cos(this._angle)) < 0.5 && (Math.cos(this._angle)  > -0.5)) && this._angle != 0 && this._angle != 180) {
+    while (
+      Math.cos(this._angle) < 0.5 &&
+      Math.cos(this._angle) > -0.5 &&
+      this._angle != 0 &&
+      this._angle != 180
+    ) {
       this._angle = Math.random() * 360;
-      console.log(this._angle);
-      console.log(Math.random());
     }
-    this._angle = 180;
     this._loopid = setInterval(this.gameLoop, Game.default_update);
   }
 
-  private endWar = async (userWin:Socket, userDefeat:Socket) => {
+  private endWar = async (userWin: Socket, userDefeat: Socket) => {
     console.log('game finish ' + this._id);
     this._user1.leave(this._id);
     this._user2.leave(this._id);
     userWin.emit('finish_game', {
       score1: this._score1,
       score2: this._score2,
-      status: 'win',
+      status: 'lose',
     });
     userDefeat.emit('finish_game', {
       score1: this._score1,
       score2: this._score2,
-      status: 'lose',
+      status: 'win',
     });
     await this._gameService.finishGame(this._id, this._score1, this._score2);
     clearInterval(this._loopid);
     this.executeFinishCallbacks();
-  }
+  };
 
-  private endBattle = (scoreWin:number) :number => {
+  private endBattle = (scoreWin: number): number => {
     scoreWin++;
     if (scoreWin != Game.default_victorygoal) {
       this._rack1y = Game.default_positionR;
@@ -190,77 +186,73 @@ export class Game {
       clearInterval(this._loopid);
       this.start();
     }
-        return scoreWin;
-  }
-
+    return scoreWin;
+  };
 
   public gameLoop = async () => {
     this._futurballx = this._ballx + Math.sin(this._angle) * this._speedball;
     this._futurbally = this._bally + Math.cos(this._angle) * this._speedball;
-    let minposition = this._minY + Game.default_rackwidth + Game.default_radiusball;
-    let maxposition = ((this._maxY - Game.default_rackwidth) - Game.default_radiusball)
-    
+    const minposition =
+      this._minY + Game.default_rackwidth + Game.default_radiusball;
+    const maxposition =
+      this._maxY - Game.default_rackwidth - Game.default_radiusball;
+
     if (this._bally <= minposition) {
       console.log(this._angle);
-      if (this._futurballx >= this._rack1y &&
-        this._futurballx <= this._rack1y + Game.default_racklenght ) {
+      if (
+        this._futurballx >= this._rack1y &&
+        this._futurballx <= this._rack1y + Game.default_racklenght
+      ) {
         this._speedball *= 1.15;
         this._angle = Math.PI - this._angle;
-        let distbar = this._futurbally - minposition;
+        const distbar = this._futurbally - minposition;
         this._futurbally -= 2 * distbar;
-        // if (this._futurballx < (this._rack2y + (Game.default_racklenght / 3)) && this._angle > 100)
-        // this._angle += 10;
-        // if (this._futurballx > (this._rack2y + ( 2 * (Game.default_racklenght / 3))) && this._angle < 170)
-        // this._angle -= 10;
-      }
-      else if (this._ballx) {
-        this._score1 = this.endBattle(this._score1)
+      } else if (this._ballx) {
+        this._score1 = this.endBattle(this._score1);
         if (this._score1 >= Game.default_victorygoal)
           this.endWar(this._user1, this._user2);
-          
       }
       console.log(this._angle);
     }
-          
+
     if (this._futurbally >= maxposition) {
-      console.log(this._angle);
-      if (this._futurballx >= this._rack2y &&
-        this._futurballx <= (this._rack2y + Game.default_racklenght)) {
+      if (
+        this._futurballx >= this._rack2y &&
+        this._futurballx <= this._rack2y + Game.default_racklenght
+      ) {
         this._speedball *= 1.15;
         this._angle = Math.PI - this._angle;
-        let distbar = this._futurbally - maxposition;
+        const distbar = this._futurbally - maxposition;
         this._futurbally -= 2 * distbar;
-
-        //if (this._futurballx < (this._rack1y + (Game.default_racklenght / 3)) && this._angle < 80)
-        //  this._angle -= 10;
-        //if (this._futurballx > (this._rack1y + ( 2 * (Game.default_racklenght / 3))) && this._angle > 280)
-        //  this._angle += 10;
-      }
-      else {
-        this._score2 = this.endBattle(this._score1)
+      } else {
+        this._score2 = this.endBattle(this._score2);
         if (this._score2 >= Game.default_victorygoal)
           this.endWar(this._user2, this._user1);
       }
       console.log(this._angle);
     }
 
-    if (this._futurballx < this._minX + Game.default_radiusball || this._futurballx > this._maxX - Game.default_radiusball) {
+    if (
+      this._futurballx < this._minX + Game.default_radiusball ||
+      this._futurballx > this._maxX - Game.default_radiusball
+    ) {
       if (this._futurballx < this._minX + Game.default_radiusball) {
-        let distbar = this._futurballx - (this._minX + Game.default_radiusball);
+        const distbar =
+          this._futurballx - (this._minX + Game.default_radiusball);
+        this._futurballx -= 2 * distbar;
+      } else {
+        const distbar =
+          this._futurballx - (this._maxX + Game.default_radiusball);
         this._futurballx -= 2 * distbar;
       }
-      else {
-        let distbar = this._futurballx - (this._maxX + Game.default_radiusball);
-        this._futurballx -= 2 * distbar;
-      }
-      this._angle = - this._angle;
+      this._angle = -this._angle;
     }
     this._ballx = this._futurballx;
     this._bally = this._futurbally;
 
     this._io.to(this._id).emit('update_game', this.getGameInfo());
   };
-    
+
   private executeFinishCallbacks() {
     console.log('Callbacks:', this._finishCallback);
     this._finishCallback.forEach((callback) => callback());
@@ -269,7 +261,7 @@ export class Game {
   onFinish(callback) {
     this._finishCallback.push(callback);
   }
-  
+
   public remake() {
     console.log('game finish ' + this._id);
     this._io.to(this._id).emit('finish_game', {
--- project/back/app/src/events/events.gateway.ts
+++ project/back/app/src/events/events.gateway.ts
@@ -37,6 +37,7 @@ export class EventsGateway
   private clients: Map<string, Socket> = new Map<string, Socket>();
   private ingame: Map<string, string> = new Map<string, string>();
   private games: Map<string, Game> = new Map<string, Game>();
+  private rematch: Map<string, boolean> = new Map<string, boolean>();
   private matchmaking: Array<Socket> = [];
   private logger: Logger = new Logger('EventsGateway');
 
@@ -398,6 +399,7 @@ export class EventsGateway
           this.clients,
         );
         if (
+          pretended_player_id != null &&
           pretended_player_id != player_id &&
           !(await this.userService.OneOfTwoBlocked(
             pretended_player_id,
@@ -436,61 +438,69 @@ export class EventsGateway
           this.check_matchmaking();
           return;
         }
-        const create_gameDTO = new CreateGameDTO();
-        create_gameDTO.user1_id = getIdFromSocket(player, this.clients);
-        create_gameDTO.user2_id = getIdFromSocket(rival, this.clients);
-        await this.userService.changeStatus(
-          create_gameDTO.user1_id,
-          UserStatus.IN_GAME,
-        );
-        await this.userService.changeStatus(
-          create_gameDTO.user2_id,
-          UserStatus.IN_GAME,
-        );
-        this.sendconnected();
-        const create_game = await this.gameService.createGame(create_gameDTO);
-        this.ingame.set(create_gameDTO.user1_id, create_game.id);
-        this.ingame.set(create_gameDTO.user2_id, create_game.id);
-        player.emit('game_found', {
-          game_id: create_game.id,
-          user: 1,
-          rival: rival.id,
-        });
-        rival.emit('game_found', {
-          game_id: create_game.id,
-          user: 2,
-          rival: player.id,
-        });
-        player.join(create_game.id);
-        rival.join(create_game.id);
-        const game = new Game(
-          create_game.id,
-          player,
-          rival,
-          this.server,
-          this.gameService,
-        );
-        this.games[game.getId()] = game;
-        game.onFinish((finishedGame) => {
-          this.logger.debug(game);
-          this.ingame.delete(getIdFromSocket(game.getUser1(), this.clients));
-          this.ingame.delete(getIdFromSocket(game.getUser2(), this.clients));
-          this.sendconnected();
-          this.logger.log(game.getId() + ' finished');
-        });
-        const tempmatchmaking = [];
-        for (const t of this.matchmaking) {
-          if (t.id != rival.id && player.id != t.id) {
-            tempmatchmaking.push(player);
-          }
+        if (
+          this.server.sockets.sockets.get(player.id) != null ||
+          this.server.sockets.sockets.get(rival.id) != null
+        ) {
+          await this.play_game(player, rival);
         }
-        this.matchmaking = tempmatchmaking;
-        this.server.to(game.getId()).emit('game_start', game.getId());
-        this.logger.log('game ' + game.getId() + ' started');
-        game.start();
       }
       i++;
     }
+  }
+
+  async play_game(player: Socket, rival: Socket) {
+    const create_gameDTO = new CreateGameDTO();
+    create_gameDTO.user1_id = getIdFromSocket(player, this.clients);
+    create_gameDTO.user2_id = getIdFromSocket(rival, this.clients);
+    await this.userService.changeStatus(
+      create_gameDTO.user1_id,
+      UserStatus.IN_GAME,
+    );
+    await this.userService.changeStatus(
+      create_gameDTO.user2_id,
+      UserStatus.IN_GAME,
+    );
+    this.sendconnected();
+    const create_game = await this.gameService.createGame(create_gameDTO);
+    this.ingame.set(create_gameDTO.user1_id, create_game.id);
+    this.ingame.set(create_gameDTO.user2_id, create_game.id);
+    this.logger.debug('game created ' + create_game.id + ' ' + create_gameDTO.user1_id + ' ' + create_gameDTO.user2_id);
+    player.emit('game_found', {
+      game_id: create_game.id,
+      user: 1,
+      rival: rival.id,
+    });
+    rival.emit('game_found', {
+      game_id: create_game.id,
+      user: 2,
+      rival: player.id,
+    });
+    player.join(create_game.id);
+    rival.join(create_game.id);
+    const game = new Game(
+      create_game.id,
+      player,
+      rival,
+      this.server,
+      this.gameService,
+    );
+    this.games[game.getId()] = game;
+    game.onFinish((finishedGame) => {
+      this.logger.debug(game);
+      this.sendconnected();
+      this.logger.log(game.getId() + ' finished');
+    });
+    const tempmatchmaking = [];
+    for (const t of this.matchmaking) {
+      if (t.id != rival.id && player.id != t.id) {
+        tempmatchmaking.push(player);
+      }
+    }
+    this.matchmaking = tempmatchmaking;
+    this.server.to(game.getId()).emit('game_start', game.getId());
+    this.logger.log('game ' + game.getId() + ' started');
+    game.start();
   }
 
   @SubscribeMessage('input_game')
@@ -526,5 +536,37 @@ export class EventsGateway
     this.matchmaking = tempmatchmaking;
     this.logger.debug('leaving matchmaking ' + id);
     client.emit('matchmaking_code', send);
+  }
+
+  @SubscribeMessage('game_finished')
+  async game_finished(client: Socket, payload: any) {
+    const rematch = payload.rematch;
+    const id = getIdFromSocket(client, this.clients);
+    this.logger.debug('game finished ' + id);
+    const game_id = this.ingame.get(id);
+    const game = this.games[game_id];
+    if (game != null) {
+      if (!rematch) {
+        this.ingame.delete(getIdFromSocket(game.getUser1(), this.clients));
+        this.ingame.delete(getIdFromSocket(game.getUser2(), this.clients));
+      } else {
+        if (this.rematch.get(game_id) == null) {
+          this.rematch.set(game_id, true);
+          if (game.getUser1().id == client.id) {
+            game.getUser2().emit('rematch', true);
+          }
+          if (game.getUser2().id == client.id) {
+            game.getUser1().emit('rematch', true);
+          }
+        } else {
+          if (
+            this.server.sockets.sockets.get(game.getUser1()) != null ||
+            this.server.sockets.sockets.get(game.getUser2()) != null
+          ) {
+            this.play_game(game.getUser1(), game.getUser2());
+          }
+        }
+      }
+    }
   }
 }
