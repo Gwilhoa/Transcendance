bd94b36502e679a7f690c6c3ade2a2ed36210743
fdaumas <florian.daumas.git@gmail.com>
2023-06-09 10:54:12 +0200

    finish profil and start conversation

--- project/front/app/src/components/chat/conversation.tsx
+++ project/front/app/src/components/chat/conversation.tsx
@@ -3,13 +3,20 @@ import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux";
 import { cookies } from "../../App";
 import { RootState } from "../../redux/store";
+import { socket } from "../utils/API";
 import "./css/sidebar.css"
 
 function Conversation() {
 	const conversationId = useSelector((state: RootState) => state.conversation.id);
 
+
+	socket.on('message_code', (data: any) => {
+		console.log(data);
+	})
+
 	useEffect(() =>{
 		console.log(conversationId);
+		socket.emit('send_message', {token: cookies.get("jwtAuthorization"), channel_id: conversationId, content: "test"});
 		axios.get(process.env.REACT_APP_IP + ':3000/channel/message/' + conversationId,
 				{
 					headers: {
