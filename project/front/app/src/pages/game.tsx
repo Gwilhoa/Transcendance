ec636bd95bd14a7a547be7f566fd19511b0ce049
gchatain <gchatain@student.42lyon.fr>
2023-06-11 17:43:43 +0200

    fix game

--- project/front/app/src/pages/game.tsx
+++ project/front/app/src/pages/game.tsx
@@ -5,17 +5,20 @@ import { socket } from '../components/utils/API';
 import Cookies from 'universal-cookie';
 import { useNavigate } from 'react-router-dom';
 import ErrorToken from '../components/IfError';
+import { openModal } from '../redux/modal/modalSlice';
 import {useDispatch, useSelector} from 'react-redux';
 import {setFinalStatus} from "../redux/game/gameSlice";
 import {RootState} from "../redux/store";
 const cookies = new Cookies();
 
+let isCall = true;
 
 interface GameProps {
   gameId: number;
 }
 
 const Game: React.FC<GameProps> = ({ gameId }) => {
+  isCall = true;
   const navigate = useNavigate();
   const [onGame, findGame] = useState(gameId);
   console.log(onGame);
@@ -26,7 +29,7 @@ const Game: React.FC<GameProps> = ({ gameId }) => {
   const [paddle1, setPaddle1] = useState(42.5 );
   const [paddle2, setPaddle2] = useState(42.5 );
   const [color1, setColor1] = useState("white");
-    const [color2, setColor2] = useState("white");
+  const [color2, setColor2] = useState("white");
   const dispatch = useDispatch();
   const finalStatus = useSelector((state: RootState) => state.finalGame.finalStatus);
 
@@ -43,10 +46,10 @@ const Game: React.FC<GameProps> = ({ gameId }) => {
       case "KeyS":
         socket.emit("input_game", {game_id: gameId, type: 1})
         break;
-        case "ArrowUp":
+      case "ArrowUp":
         socket.emit("input_game", {game_id: gameId, type: 0})
         break;
-        case "ArrowDown":
+      case "ArrowDown":
         socket.emit("input_game", {game_id: gameId, type: 1})
     }
   };
@@ -81,47 +84,44 @@ const Game: React.FC<GameProps> = ({ gameId }) => {
     });
   }, []);
 
- useEffect(() => {
-   socket.on('finish_game', (any) => {
-     console.log('finish game');
-     const content = {status: any.status, adversary: any.adversary, score1: any.score1, score2: any.score2};
-     dispatch(setFinalStatus(content));
-     navigate('/endGame');
-     return;
-   });
-   socket.on('game_found', (data) => {
-     if (data.game_id == null) {
-       console.log('error while game creating');
-     }
-     if (data.user == 1) {
-       setColor1('blue');
-       setColor2('red');
-     } else {
-       setColor2('blue');
-       setColor1('red');
-     }
-   });
+  socket.on('finish_game', (any) => {
+    if (isCall) {
+      const content = {status: any.status, adversary: any.adversary, score1: any.score1, score2: any.score2};
+      dispatch(setFinalStatus(content));
+      navigate('/endGame');
+      isCall = !isCall;
+    }
+  });
+  socket.on('game_found', (data) => {
+    if (data.game_id == null) {
+      console.log('error while game creating');
+    }
+    if (data.user == 1) {
+      setColor1('blue');
+      setColor2('red');
+    } else {
+      setColor2('blue');
+      setColor1('red');
+    }
+  });
 
-   socket.on('create_game', (any) => {
-     console.log("WESH")
-     console.log(any);
-   })
+  socket.on('create_game', (any) => {
+    console.log("WESH")
+    console.log(any);
+  })
 
-   socket.on('update_game', (data) => {
-     setScore1(data.score1);
-     setScore2(data.score2);
-     setBall({x: (data.ballx), y: (data.bally)});
-     setPaddle2(data.rack2y)
-     setPaddle1(data.rack1y);
-     if (!onGame) {
-       findGame(1);
-     }
-   });
-
- }, [socket]);
 
   useEffect(() => {
-
+    socket.on('update_game', (data) => {
+      setScore1(data.score1);
+      setScore2(data.score2);
+      setBall({x: (data.ballx), y: (data.bally)});
+      setPaddle2(data.rack2y)
+      setPaddle1(data.rack1y);
+      if (!onGame) {
+        findGame(1);
+      }
+    });
 
     return () => {
       socket.off('update_game');
@@ -130,35 +130,35 @@ const Game: React.FC<GameProps> = ({ gameId }) => {
   }, [onGame]);
 
   return (
-    <>
-    <ErrorToken />
-      {onGame == 0 &&
-        <div className='center-page'>
-          <h2 style={{color: 'white'}}> Searching players... </h2>
-          <animated.img src={"https://pic.onlinewebfonts.com/svg/img_155544.png"} className={"gameimg"} style={spinnerAnimation}></animated.img>
-        </div>
-      }
-
-      {onGame == 1 &&
-        <>
-          <canvas
-          ref={canvasRef}
-          className="game-canvas"
-          > </canvas>
-          <div className="parentscore">
-            <div className="score">
-              <h1>
-
-              {score1 + " | " + score2}
-              </h1>
+      <>
+        <ErrorToken />
+        {onGame == 0 &&
+            <div className='center-page'>
+              <h2 style={{color: 'white'}}> Searching players... </h2>
+              <animated.img src={"https://pic.onlinewebfonts.com/svg/img_155544.png"} className={"gameimg"} style={spinnerAnimation}></animated.img>
             </div>
-          </div>
-          <div className="paddle1" style={{ top: paddle1 + '%', background: color1 }} />
-          <div className="paddle2" style={{ top: paddle2 + '%', background: color2 }} />
-          <div className="ball" style={{ top: ball.x + '%', left: ball.y - 1 + '%'}} />
-        </>
-      }
-    </>
+        }
+
+        {onGame == 1 &&
+            <>
+              <canvas
+                  ref={canvasRef}
+                  className="game-canvas"
+              > </canvas>
+              <div className="parentscore">
+                <div className="score">
+                  <h1>
+
+                    {score1 + " | " + score2}
+                  </h1>
+                </div>
+              </div>
+              <div className="paddle1" style={{ top: paddle1 + '%', background: color1 }} />
+              <div className="paddle2" style={{ top: paddle2 + '%', background: color2 }} />
+              <div className="ball" style={{ top: ball.x + '%', left: ball.y + '%'}} />
+            </>
+        }
+      </>
 
   );
 }
@@ -168,4 +168,3 @@ const Game: React.FC<GameProps> = ({ gameId }) => {
 
 
 export default Game;
-
