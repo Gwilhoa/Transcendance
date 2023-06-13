d845017c0956da7798d2234896382fbbb19243ba
Henri Penaud <henrialsace@laposte.net>
2023-06-13 20:07:29 +0200

    css continuesend message

--- project/front/app/src/components/chat/input/sendmessage.tsx
+++ project/front/app/src/components/chat/input/sendmessage.tsx
@@ -1,3 +1,5 @@
+
+import '../css/sendMessageBar.css'
 import React from 'react'
 import {useEffect, useState} from 'react';
 import SocketSingleton from '../../../socket';
@@ -18,9 +20,9 @@ export default function SendMessage(){
 
 
 	return (
-		<>
-			<input type='text' placeholder='Message' onChange={(e) => setMessage(e.target.value)}/>
-			<button onClick={handleSendMessage}>Send</button>
-		</>
+		<div className='chat-input-send-bar'>
+			<input className='chat-input-message' type='text' placeholder='Message' onChange={(e) => setMessage(e.target.value)}/>
+			<button className='chat-button-send-message' onClick={handleSendMessage}>{'>'}</button>
+		</div>
 	)
 }
