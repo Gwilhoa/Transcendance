e2e2e2614b373d9678289a25904f06f5eef45baa
fdaumas <florian.daumas.git@gmail.com>
2023-06-26 08:41:56 +0200

    add waiting in connection

--- project/front/app/src/pages/authenticate.tsx
+++ project/front/app/src/pages/authenticate.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import {setErrorLocalStorage} from '../components/IfError'
 import {useNavigate} from 'react-router-dom';
@@ -11,22 +11,32 @@ export function TokenPage() {
 	}
 	const navigate = useNavigate();
 
-	axios.get(process.env.REACT_APP_IP + ':3000/auth/2fa/is2FA', {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	})
-		.then((response) => {
-			if (response.data == false)
-				navigate('/authenticate/NotTwoFa');
-			else
-				navigate('/authenticate/TwoFa');
-		})
-		.catch((error) => {
-			setErrorLocalStorage('Error ' + error.response.status);
-			console.error(error);
-			navigate('/Error');
-		});
+	useEffect(() => {
+		const interval = setInterval(()=>{
+			if (localStorage.getItem('tenMinToken') != null) {
+				axios.get(process.env.REACT_APP_IP + ':3000/auth/2fa/is2FA', {
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				})
+					.then((response) => {
+						if (response.data == false)
+							navigate('/authenticate/NotTwoFa');
+						else
+							navigate('/authenticate/TwoFa');
+					})
+					.catch((error) => {
+						setErrorLocalStorage('Error ' + error.response.status);
+						console.error(error);
+						navigate('/Error');
+					});
+			}
+		}, 500);
+
+		return () => {
+			clearInterval(interval);		
+		};
+	}, []);
 
 	return (
 		<>
