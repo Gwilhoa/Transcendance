c7d4032e45e04243ab7ae97abd7158c8b8ba6c90
fdaumas <florian.daumas.git@gmail.com>
2023-06-27 13:53:45 +0200

    check socket

--- project/front/app/src/pages/waiting.tsx
+++ project/front/app/src/pages/waiting.tsx
@@ -1,9 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
+import { setErrorLocalStorage } from '../components/IfError';
+import SocketSingleton from '../socket';
 
 const Waiting = () => {
 	const [message, setMessage] = useState<string>('waiting.');
 	const navigate = useNavigate();
+
+	const socketInstance = SocketSingleton.getInstance();
+	const socket = socketInstance.getSocket();
+	
+	useEffect(() => {
+		socket.on('connection_error', () => {
+			setErrorLocalStorage('unauthorized')
+			navigate('/error');
+		});
+
+		return () => {
+			socket.off('connection_error');
+		};
+	}, [navigate]);
 
 	useEffect(() => {
 		const interval = setInterval(() => {
