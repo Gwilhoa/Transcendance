ac91f0c0d664133036643d5d6b312ed7c90a3d3e
gchatain <gchatain@student.42lyon.fr>
2023-06-27 13:48:58 +0200

    suppression comment

--- project/back/app/src/auth/auth.controller.ts
+++ project/back/app/src/auth/auth.controller.ts
@@ -8,7 +8,6 @@ import {
   Res,
   UseGuards,
 } from '@nestjs/common';
-// import { UserService } from '../user/user.service';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard, JwtIsAuthGuard } from './guard/jwt.guard';
 import { GetUser } from './decorator/auth.decorator';
